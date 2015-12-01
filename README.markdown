6db8640657fb26bab778f07ec402f808dd8a3ddd
Matt Rideout <mrideout@windserve.com>
2015-12-01 10:32:00 -0500

    Update documentation to point to new URLs on docs.drh.net

--- README.markdown
+++ README.markdown
@@ -1,11 +1,4 @@
-<!-- START doctoc generated TOC please keep comment here to allow auto update -->
-<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
-**Table of Contents**
-
-- [GreenArrow API](#greenarrow-api)
-
-<!-- END doctoc generated TOC please keep comment here to allow auto update -->
-
 # GreenArrow API
 
-This repository contains the public API for DRH Internet's products.
+This page has moved to [http://docs.drh.net/greenarrow-studio/API-V2/](http://docs.drh.net/greenarrow-studio/API-V2/).
+
--- studio/examples/create_a_new_subscriber.php
+++ studio/examples/create_a_new_subscriber.php
@@ -1,38 +1,4 @@
-<?php
+# Create a New Subscriber PHP Example
 
-// API Configuration
-$api_key = 'API_KEY_HERE_FROM_WEB_UI';
-$api_url = 'http://example.com/ga/api';
- 
-// Subscriber data
-$listID = 5;
-$email = 'customer1@example.com';
+This page has moved to http://docs.drh.net/greenarrow-studio/API-V2/Examples/Create-a-New-Subscriber.
 
- 
-// subscriberAdd.php - Example usage of the GreenArrow Studio
-// API's subscriberAdd method.
- 
-// API request.
-$request = array(
-    'email'  => $email,
-    'confirmed' => true,
-    'email_format' => 'html',
-    'status' => 'active',
-    'subscribe_time' => date('c'),
-    'subscribe_ip' => null,
-);
- 
-$request_url = "$api_url/mailing_lists/$listID/subscribers";
- 
-print "\nRequesting\n$request_url\n";
- 
-$request_handle = curl_init($request_url);
-curl_setopt($request_handle, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($request_handle, CURLOPT_USERPWD, "1:$api_key");
-curl_setopt($request_handle, CURLOPT_POST, true);
-curl_setopt($request_handle, CURLOPT_POSTFIELDS, json_encode($request));
-curl_setopt($request_handle, CURLOPT_RETURNTRANSFER, true);
-$request_result = curl_exec($request_handle);
- 
-print "\nThe results are:\n";
-print_r(json_decode($request_result));
